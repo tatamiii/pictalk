99dfc3ea0630ca5fc6e2f7e750debcc2b5e5bf20
tatamiii <sousuke2828@gmail.com>
2020-09-23 17:47:06 +0900

    インスタンス変数の記述の修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: "posts#index"
-  resources :posts, only: [:index, :new, :create]
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
