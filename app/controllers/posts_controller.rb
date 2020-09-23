731bfcb0a23f032a748f6e3c10579f0309bd18d3
tatamiii <sousuke2828@gmail.com>
2020-09-23 17:42:56 +0900

    Createアクションをコントローラーに定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,14 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    Post.create(post_params)
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:name, :image, :text)
+  end
   
 end
