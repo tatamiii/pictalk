d9f0bdc032a6c2487cdd0c35fbe25eea711308fb
tatamiii <sousuke2828@gmail.com>
2020-09-24 19:44:42 +0900

    コメント投稿後、リダイレクトするように記述

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
-    redirect_to root_path
+    comment = Comment.create(comment_params)
+    redirect_to "/posts/#{comment.post.id}"
   end
 
   private
